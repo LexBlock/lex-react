ac84884d5d5ffee1e71bc9024618ba9c2f23e348
alibama <adp6j@virginia.edu>
2023-09-07 17:34:56 +0000

    just some small edits

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,22 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Wyoming Stablecoin Review",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Technical and legal interpretations with a proof-of-concept user interface.",
+    url: "https://lexdao.org",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Policy Support",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Get content.",
+    url: "https://github.com/",
   },
   {
-    title: "My Resume Site",
+    title: "Building community",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Directory of LexDAO.",
+    url: "https://github.com/",
   },
   {
     title: "GitHub Codespaces and github.dev",
