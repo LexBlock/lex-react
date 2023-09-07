97f01dc9130d4292450bdb739dc5880766c0130b
alibama <adp6j@virginia.edu>
2023-09-07 12:43:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "LexBlock dApplications",
+  title: "Web2 & Web3 integrations",
+  email: "lexblock@gmail.com",
+  gitHub: "https://github.com/LexBlock/l",
+  instagram: "lexblock",
+  linkedIn: "lexblock",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "lexblock",
+  youTube: "lexblock",
 };
 
 const primaryColor = "#4E567E";
